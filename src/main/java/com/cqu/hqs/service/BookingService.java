f6e6773d3fa29069e1ec4caadeec8c14dab15a45
lakshitha12192752 <12192752@cqumail.com>
2023-09-13 19:05:23 +1000

    Created controller functions for booking rooms

--- src/main/java/com/cqu/hqs/service/BookingService.java
+++ src/main/java/com/cqu/hqs/service/BookingService.java
@@ -4,7 +4,10 @@ import com.cqu.hqs.Repository.BookingRepository;
 import com.cqu.hqs.dto.BookingResponseDto;
 import com.cqu.hqs.entity.Booking;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingService {
@@ -22,5 +25,12 @@ public class BookingService {
     public BookingResponseDto mapToResponseDto(Booking booking) {
         BookingResponseDto bookingResponseDto = mapper.map(booking, BookingResponseDto.class);
         return bookingResponseDto;
+    }
+
+    public List<BookingResponseDto> getAllBookings() {
+//        List<Booking> bookings = bookingRepository.findAll();
+//        return bookings.stream().map(booking -> mapToResponseDto(booking)).collect((Collectors.toList()));
+        List<Booking> bookings = bookingRepository.findAll(Sort.by(Sort.Direction.DESC, "createdDate"));
+        return bookings.stream().map(booking -> mapToResponseDto(booking)).collect((Collectors.toList()));
     }
 }
